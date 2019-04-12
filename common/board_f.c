fe017c3185ea9cbc8f188aec5ea042c9d26ca770
sun qihao <qihao_sun@163.com>
2019-04-12 09:04:02 -0700

    need to test nandflash driver

--- common/board_f.c
+++ common/board_f.c
@@ -512,8 +512,9 @@ static int reserve_uboot(void)
 	 * reserve memory for U-Boot code, data & bss
 	 * round down to next 4 kB limit
 	 */
-	gd->relocaddr -= gd->mon_len;
-	gd->relocaddr &= ~(4096 - 1);
+	/*gd->relocaddr -= gd->mon_len;
+	gd->relocaddr &= ~(4096 - 1);*/
+	gd->relocaddr = CONFIG_SYS_TEXT_BASE;
 #ifdef CONFIG_E500
 	/* round down to next 64 kB limit so that IVPR stays aligned */
 	gd->relocaddr &= ~(65536 - 1);
